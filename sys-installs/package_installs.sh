1dc7d3d3b08afddf6e3295fecf87f363a465b827
jriverahmla <rivera.61@wright.edu>
2025-04-24 15:13:52 -0400

    Update package_installs.sh
    Menu is now called from method, script no longer requirements an argument, software is specified to Ansible, script installs software now that $software is specified to Ansible, script does checks and gives feedback after running, now tells the user that software is installed.

--- sys-installs/package_installs.sh
+++ sys-installs/package_installs.sh
@@ -1,67 +1,47 @@
-# Full Script
-
 #!/bin/bash
 
 ## Check if running as root
-```
 if [[ $EUID -ne 0 ]]; then
   echo "Please run as root."
   exit 1
 fi
-```
+
 ## Check if apt is available
-```
 if command -v apt >/dev/null; then
     echo 'apt is available'
-
-fi
-```
-## User's choice to install or not
-```
-prompt_for_input() {
-    while true; do
-        echo -n "Do you want to update the system? (y/n/c): "
-        read -r response
-        case "$response" in
-            [Yy]* )
-                echo "You selected Yes."
-                return 0
-                ;;
-            [Nn]* )
-                echo "You selected No."
-                return 1
-                ;;
-            [Cc]* )
-                echo "You selected Cancel."
-                return 2
-                ;;
-            * )
-                echo "Invalid input. Please enter y, n, or c."
-                ;;
-        esac
-    done
-}
-```
-## Checks name of software or software exists
-```
-dpkg -s $1 &> /dev/null
-
-if [ $? -eq 0 ]; then
-    echo "Package $1 is installed!"
+    prompt_for_input
+    case $? in
+        0)
+            echo "Updating the system..."
+            sudo apt update && sudo apt upgrade -y
+            ;;
+        1)
+            echo "Skipping system update."
+            ;;
+        2)
+            echo "Update canceled."
+            exit 0
+            ;;
+    esac
 else
-    echo "Package $1 is NOT installed!"
+    echo 'apt is not available'
+    exit 1
 fi
-```
-## Shadow downloads software
 
-```echo "Software is installing now```
-```sudo apt install -y $software```
+## Define the software to be installed
+software="ansible"
 
-## User prompt for install
-```
-if [[ $? -eq 0 ]]; then
+## Check if the software is installed
+if dpkg -s "$software" &> /dev/null; then
+    echo "Package $software is installed!"
+else
+    echo "Package $software is NOT installed!"
+fi
+
+## Install the software
+echo "Software is installing now"
+if sudo apt install -y "$software"; then
   echo "Installation complete. You can run '$software' to try it."
 else
   echo "Installation failed."
 fi
-```
